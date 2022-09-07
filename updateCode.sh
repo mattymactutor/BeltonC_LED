b951fab94c96e2269dd1ea0b877005bca95e18b1
mattymactutor <96496318+mattymactutor@users.noreply.github.com>
2022-09-07 15:06:47 -0700

    Update updateCode.sh

--- updateCode.sh
+++ updateCode.sh
@@ -1,11 +1,13 @@
 #1/bin/bash
 
 cd BeltonC_LED
+echo "Downloading new code files from Github..."
+git pull
 echo "Building qmake..."
 sleep 1
 qmake -o Makefile BeltonC_LEDController.pro
 echo "Building runnable files..."
 sleep 1
 make
-echo "Finished...closing in 15 seconds"
+echo "Finished...closing in 15 seconds. You can now double click -Run LEDS Controller- on the desktop"
 sleep 15
