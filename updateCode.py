e13752153b7f2bff70e5af93ff292e0877de385c
mattymactutor <96496318+mattymactutor@users.noreply.github.com>
2022-11-02 22:10:46 -0700

    Update updateCode.py

--- updateCode.py
+++ updateCode.py
@@ -7,10 +7,10 @@ os.system("git clone https://github.com/mattymactutor/BeltonC_LED")
 time.sleep(2)
 os.system("cd /home/pi/BeltonC_LED")
 print("Building the make file with qmake...");
-os.system.("qmake -o Makefile BeltonC_LEDController.pro")
+os.system("qmake -o Makefile BeltonC_LEDController.pro")
 time.sleep(0.5);
 print("Making new program...");
-os.system.("make")
+os.system("make")
 time.sleep(0.5);
 print("Running now...")
 os.system("./BeltonC_LEDController")
