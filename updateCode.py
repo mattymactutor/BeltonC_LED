b9273cd5869690e546059ddd3b20e8eff9e3f140
mattymactutor <96496318+mattymactutor@users.noreply.github.com>
2022-11-02 22:03:19 -0700

    Add files via upload

--- updateCode.py
+++ updateCode.py
@@ -2,13 +2,10 @@ import os
 import time
 
 os.system("cd")
+os.system("sudo rm -r BeltonC_LED")
+os.system("git clone https://github.com/mattymactutor/BeltonC_LED")
+time.sleep(2)
 os.system("cd /home/pi/BeltonC_LED")
-print("Removing old files...")
-os.system("make clean")
-time.sleep(0.5)
-print("Downloading new files...")
-os.system("git pull");
-time.sleep(0.5)
 print("Building the make file with qmake...");
 os.system.("qmake -o Makefile BeltonC_LEDController.pro")
 time.sleep(0.5);
